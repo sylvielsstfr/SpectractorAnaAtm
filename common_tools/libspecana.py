a748d1dad52f06cf5f1d71eb65313219b4c13647
Sylvie Dagoret-Campagne <dagoret@lal.in2p3.fr>
2018-05-08 20:20:25 +0200

    FitSpectra.ipynb used to find the timeshift

--- common_tools/libspecana.py
+++ common_tools/libspecana.py
@@ -41,4 +41,157 @@ def Convert_InFloat(arr_str):
     arr=[ atof(x.replace(",",".")) for x in arr_str]
     arr=np.array(arr)
     return arr
+#-----------------------------------------------------------------------------------------
+def roll_zeropad(a, shift, axis=None):
+    """
+    Roll array elements along a given axis.
+
+    Elements off the end of the array are treated as zeros.
+
+    Parameters
+    ----------
+    a : array_like
+        Input array.
+    shift : int
+        The number of places by which elements are shifted.
+    axis : int, optional
+        The axis along which elements are shifted.  By default, the array
+        is flattened before shifting, after which the original
+        shape is restored.
+
+    Returns
+    -------
+    res : ndarray
+        Output array, with the same shape as `a`.
+
+    See Also
+    --------
+    roll     : Elements that roll off one end come back on the other.
+    rollaxis : Roll the specified axis backwards, until it lies in a
+               given position.
+
+    Examples
+    --------
+    >>> x = np.arange(10)
+    >>> roll_zeropad(x, 2)
+    array([0, 0, 0, 1, 2, 3, 4, 5, 6, 7])
+    >>> roll_zeropad(x, -2)
+    array([2, 3, 4, 5, 6, 7, 8, 9, 0, 0])
+
+    >>> x2 = np.reshape(x, (2,5))
+    >>> x2
+    array([[0, 1, 2, 3, 4],
+           [5, 6, 7, 8, 9]])
+    >>> roll_zeropad(x2, 1)
+    array([[0, 0, 1, 2, 3],
+           [4, 5, 6, 7, 8]])
+    >>> roll_zeropad(x2, -2)
+    array([[2, 3, 4, 5, 6],
+           [7, 8, 9, 0, 0]])
+    >>> roll_zeropad(x2, 1, axis=0)
+    array([[0, 0, 0, 0, 0],
+           [0, 1, 2, 3, 4]])
+    >>> roll_zeropad(x2, -1, axis=0)
+    array([[5, 6, 7, 8, 9],
+           [0, 0, 0, 0, 0]])
+    >>> roll_zeropad(x2, 1, axis=1)
+    array([[0, 0, 1, 2, 3],
+           [0, 5, 6, 7, 8]])
+    >>> roll_zeropad(x2, -2, axis=1)
+    array([[2, 3, 4, 0, 0],
+           [7, 8, 9, 0, 0]])
+
+    >>> roll_zeropad(x2, 50)
+    array([[0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0]])
+    >>> roll_zeropad(x2, -50)
+    array([[0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0]])
+    >>> roll_zeropad(x2, 0)
+    array([[0, 1, 2, 3, 4],
+           [5, 6, 7, 8, 9]])
+
+    """
+    a = np.asanyarray(a)
+    if shift == 0: return a
+    if axis is None:
+        n = a.size
+        reshape = True
+    else:
+        n = a.shape[axis]
+        reshape = False
+    if np.abs(shift) > n:
+        res = np.zeros_like(a)
+    elif shift < 0:
+        shift += n
+        zeros = np.zeros_like(a.take(np.arange(n-shift), axis))
+        res = np.concatenate((a.take(np.arange(n-shift,n), axis), zeros), axis)
+    else:
+        zeros = np.zeros_like(a.take(np.arange(n-shift,n), axis))
+        res = np.concatenate((zeros, a.take(np.arange(n-shift), axis)), axis)
+    if reshape:
+        return res.reshape(a.shape)
+    else:
+        return res
 #----------------------------------------------------------------------------------------
+def smooth(x,window_len=11,window='hanning'):
+    """smooth the data using a window with requested size.
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    
+    input:
+        x: the input signal 
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+        
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+    
+    see also: 
+    
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+ 
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: 
+        return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    #return y
+    if len(x)%2==0: # even case
+        return y[(window_len/2):-(window_len/2)] 
+    else:           #odd case
+        return y[(window_len/2-1):-(window_len/2)] 
