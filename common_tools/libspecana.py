8cba9976402e77bbf0d4740fa5cab89ad7b79dd3
Sylvie Dagoret-Campagne <dagoret@lal.in2p3.fr>
2018-05-10 22:17:55 +0200

    put functions in lib file

--- common_tools/libspecana.py
+++ common_tools/libspecana.py
@@ -5,6 +5,19 @@ Created on Mon May  7 13:52:53 2018
 
 @author: dagoret
 """
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+import pandas as pd
+import glob
+from matplotlib.backends.backend_pdf import PdfPages 
+from scipy import interpolate
+from astropy.io import fits
+import sys,os
+PATH_SPECTRACTORSIM='../../SpectractorSim'
+sys.path.append(PATH_SPECTRACTORSIM)
+from spectractorsim import *
+
 import re,os
 import numpy as np
 from locale import *
@@ -195,3 +208,373 @@ def smooth(x,window_len=11,window='hanning'):
         return y[(window_len/2):-(window_len/2)] 
     else:           #odd case
         return y[(window_len/2-1):-(window_len/2)] 
+    
+#---------------------------------------------------------------------------------
+#  GetDisperserTransmission
+#-------------------------------------------------------------------------------
+def PlotSpectra(the_filelist,the_obs,the_searchtag,wlshift,the_title,FLAG_WL_CORRECTION,Flag_corr_wl=False):
+
+    jet =plt.get_cmap('jet') 
+    VMAX=len(the_filelist)
+    cNorm  = colors.Normalize(vmin=0, vmax=VMAX)
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=jet)
+    
+    the_selected_indexes=the_obs["index"].values  # get the array of index for that disperser
+    
+    plt.figure(figsize=(10,8))
+    num=0
+    for the_file in the_filelist:
+        num=num+1
+        idx=get_index_from_filename(the_file,the_searchtag)
+        if idx in the_selected_indexes:
+            if FLAG_WL_CORRECTION and Flag_corr_wl:
+                time_correction=wlshift[wlshift["index"]==idx].loc[:,"wlshift"].values[0]
+            else:
+                time_correction=0
+            
+            hdu = fits.open(the_file)
+            data=hdu[0].data
+            wl=data[0]+time_correction
+            fl=data[1]
+            colorVal = scalarMap.to_rgba(num,alpha=1)
+            plt.plot(wl,fl,c=colorVal,label=str(idx))
+    plt.grid()    
+    plt.title(the_title)
+    plt.xlabel("$\lambda$ (nm)")   
+    #plt.legend()
+#---------------------------------------------------------------------------------------------
+def PlotSpectraRatioDataDivSim(the_filelist,path_tosims,the_obs,the_searchtag,wlshift,the_title,
+                     FLAG_WL_CORRECTION,Flag_corr_wl=False,XMIN=400,XMAX=1000.,YMIN=0,YMAX=0):
+    
+    jet = plt.get_cmap('jet') 
+    cNorm  = colors.Normalize(vmin=0, vmax=len(the_filelist))
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=jet)
+    
+    the_selected_indexes=the_obs["index"].values  # get the array of index for that disperser
+    
+    plt.figure(figsize=(10,8))
+    num=0
+    
+    for the_file in the_filelist:  # loop on reconstruted spectra
+        num+=1
+        idx=get_index_from_filename(the_file,the_searchtag)
+        if idx in the_selected_indexes:   
+            
+            if FLAG_WL_CORRECTION and Flag_corr_wl:
+                wl_correction=wlshift[wlshift["index"]==idx].loc[:,"wlshift"].values[0]
+            else:
+                wl_correction=0
+            
+            
+            # check if tthe index is in the disperser indexes 
+            basefn=os.path.basename(the_file)                  # basename of reconstruced spectra
+            basefn2=basefn.replace('reduc','specsim')  # reconstruct the simulation filename
+            the_filesim=os.path.join(path_tosims,basefn2)  # add the path for the simulated file
+            
+            hdu1 = fits.open(the_file)
+            data1=hdu1[0].data
+            wl1=data1[0]+wl_correction
+            fl1=data1[1]
+            err1=data1[2]
+            
+            # extend range for (wl1,fl1)
+            wl1=np.insert(wl1,0,WL[0])
+            fl1=np.insert(fl1,0,0.)
+            err1=np.insert(err1,0,0.)
+            
+            wl1=np.append(wl1,WL[-1])
+            fl1=np.append(fl1,0.)
+            err1=np.append(err1,0.)
+            
+            hdu2 = fits.open(the_filesim)
+            data2=hdu2[0].data
+            wl2=data2[0]
+            fl2=data2[1]
+            
+            func = interpolate.interp1d(wl1, fl1)
+            efunc = interpolate.interp1d(wl1, err1)
+            
+            fl0=func(WL)
+            er0=efunc(WL)
+            
+            ratio=fl0/fl2
+            eratio=er0/fl2
+            
+            colorVal = scalarMap.to_rgba(num)
+            #plt.plot(WL,ratio,'-',color=colorVal)
+            plt.fill_between(WL,y1=ratio-1.96*eratio,y2=ratio+1.96*eratio,facecolor='grey',alpha=0.5)
+            plt.errorbar(WL,ratio,yerr=eratio,fmt = '-',markersize = 1,color=colorVal,zorder = 300,antialiased = True)
+            
+            
+            the_Y=ratio
+            sel_iii=np.where(np.logical_and(WL>=XMIN,WL<=XMAX))[0]
+            the_Y_max=the_Y[sel_iii].max()*1.5
+            the_Y_min=the_Y[sel_iii].min()/1.5
+            
+            
+    plt.xlim(XMIN,XMAX)
+    
+    if YMIN==0 and YMAX==0 :
+        plt.ylim(the_Y_min,the_Y_max)
+    else:
+        plt.ylim(0,YMAX)
+        
+    plt.grid()    
+    plt.title(the_title)
+    plt.xlabel("$\lambda$ (nm)")   
+    plt.ylabel("spectra ratio")  
+#-----------------------------------------------------------------------------------------
+def PlotSpectraLogRatioDataDivSim(the_filelist,path_tosims,the_obs,the_searchtag,wlshift,the_title,FLAG_WL_CORRECTION,Flag_corr_wl=False,XMIN=400,XMAX=1000.,YMIN=0,YMAX=0):
+    
+    jet = cm = plt.get_cmap('jet') 
+    cNorm  = colors.Normalize(vmin=0, vmax=len(the_filelist))
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=jet)
+    
+    the_selected_indexes=the_obs["index"].values  # get the array of index for that disperser
+    
+    plt.figure(figsize=(10,8))
+    
+    num=0
+    for the_file in the_filelist:  # loop on reconstruted spectra
+        num+=1
+        idx=get_index_from_filename(the_file,the_searchtag)
+        
+        if FLAG_WL_CORRECTION and Flag_corr_wl:
+                wl_correction=wlshift[wlshift["index"]==idx].loc[:,"wlshift"].values[0]
+        else:
+                wl_correction=0
+        
+        
+        if idx in the_selected_indexes:                # check if tthe index is in the disperser indexes 
+            basefn=os.path.basename(the_file)                  # basename of reconstruced spectra
+            basefn2=basefn.replace('reduc','specsim')  # reconstruct the simulation filename
+            the_filesim=os.path.join(path_tosims,basefn2)  # add the path for the simulated file
+            
+            hdu1 = fits.open(the_file)
+            data1=hdu1[0].data
+            wl1=data1[0]+wl_correction
+            fl1=data1[1]
+            err1=data1[2]
+            
+            # extend range for (wl1,fl1)
+            wl1=np.insert(wl1,0,WL[0])
+            fl1=np.insert(fl1,0,0.)
+            err1=np.insert(err1,0,0.)
+            
+            wl1=np.append(wl1,WL[-1])
+            fl1=np.append(fl1,0.)
+            err1=np.append(err1,0.)
+            
+            hdu2 = fits.open(the_filesim)
+            data2=hdu2[0].data
+            wl2=data2[0]
+            fl2=data2[1]
+            
+            func = interpolate.interp1d(wl1, fl1)
+            efunc = interpolate.interp1d(wl1, err1)
+            
+            fl0=func(WL)
+            er0=efunc(WL)
+          
+            #the_Y=2.5*(np.log10(fl0)-np.log10(fl2))
+            the_Y=fl0/fl2
+            the_Y_err=er0/fl2
+            
+            colorVal = scalarMap.to_rgba(num)
+            #plt.semilogy(WL,the_Y,'o',color=colorVal)
+            plt.semilogy(WL,the_Y,'-',color=colorVal)
+            
+            sel_iii=np.where(np.logical_and(WL>=XMIN,WL<=XMAX))[0]
+            the_Y_max=np.max(the_Y[sel_iii])*3.0
+            the_Y_min=np.min(the_Y[sel_iii])/3.0
+          
+            
+            
+    plt.xlim(XMIN,XMAX)
+    
+    if YMIN==0 and YMAX==0 :
+        plt.ylim(the_Y_min,the_Y_max)
+    else:
+        plt.ylim(YMIN,YMAX)
+    
+    plt.grid()    
+    plt.title(the_title)
+    plt.xlabel("$\lambda$ (nm)")  
+    plt.ylabel("Spectra ratio")  
+    #plt.grid(True,which="majorminor",ls="-", color='0.65')
+    #plt.grid(True,which="both",ls="-")
+    plt.grid(b=True, which='major', color='k', linestyle='-',lw=1)
+    plt.grid(b=True, which='minor', color='grey', linestyle='--',lw=0.5)
+#--------------------------------------------------------------------------------- 
+def PlotSpectraRatioDataDivSimSmooth(the_filelist,path_tosims,the_obs,the_searchtag,wlshift,the_title,FLAG_WL_CORRECTION,Flag_corr_wl=False,XMIN=400,XMAX=1000.,YMIN=0,YMAX=0):
+    
+    jet =plt.get_cmap('jet') 
+    VMAX=len(the_filelist)
+    cNorm  = colors.Normalize(vmin=0, vmax=VMAX)
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=jet)
+    
+    the_selected_indexes=the_obs["index"].values  # get the array of index for that disperser
+    
+    plt.figure(figsize=(10,8))
+    num=0
+    for the_file in the_filelist:  # loop on reconstruted spectra
+        num+=1
+        idx=get_index_from_filename(the_file,the_searchtag)
+        if idx in the_selected_indexes:   
+            
+            if FLAG_WL_CORRECTION and Flag_corr_wl:
+                wl_correction=wlshift[wlshift["index"]==idx].loc[:,"wlshift"].values[0]
+            else:
+                wl_correction=0
+            
+            
+            # check if tthe index is in the disperser indexes 
+            basefn=os.path.basename(the_file)                  # basename of reconstruced spectra
+            basefn2=basefn.replace('reduc','specsim')  # reconstruct the simulation filename
+            the_filesim=os.path.join(path_tosims,basefn2)  # add the path for the simulated file
+            
+            hdu1 = fits.open(the_file)
+            data1=hdu1[0].data
+            wl1=data1[0]+wl_correction
+            fl1=data1[1]
+            err1=data1[2]
+            
+            # extend range for (wl1,fl1)
+            wl1=np.insert(wl1,0,WL[0])
+            fl1=np.insert(fl1,0,0.)
+            err1=np.insert(err1,0,0.)
+            
+            wl1=np.append(wl1,WL[-1])
+            fl1=np.append(fl1,0.)
+            err1=np.append(err1,0.)
+            
+            hdu2 = fits.open(the_filesim)
+            data2=hdu2[0].data
+            wl2=data2[0]
+            fl2=data2[1]
+            
+            func = interpolate.interp1d(wl1, fl1)
+            efunc = interpolate.interp1d(wl1, err1) 
+            
+            fl0=func(WL)
+            er0=efunc(WL)
+            
+            f1_smooth=smooth(fl0,window_len=21)
+            f2_smooth=smooth(fl2,window_len=21)
+            
+            ef1_smooth=smooth(er0,window_len=21)
+            
+            the_Y=f1_smooth/f2_smooth
+            the_Y_err=ef1_smooth/f2_smooth
+            
+            sel_iii=np.where(np.logical_and(WL>=XMIN,WL<=XMAX))[0]
+            the_Y_max=the_Y[sel_iii].max()*1.5
+            
+            colorVal = scalarMap.to_rgba(num,alpha=1)
+            
+            plt.plot(WL,the_Y,color=colorVal)
+            plt.errorbar(WL,the_Y,yerr=the_Y_err,fmt = 'o',markersize = 1,color=colorVal,zorder = 300,antialiased = True)
+            
+            
+    plt.xlim(XMIN,XMAX)
+    
+    if YMIN==0 and YMAX==0 :
+        plt.ylim(0.,the_Y_max)
+    else:
+        plt.ylim(YMIN,YMAX)
+        
+    plt.grid()    
+    plt.title(the_title)
+    plt.xlabel("$\lambda$ (nm)")   
+    plt.ylabel("spectra ratio")  
+#----------------------------------------------------------------------------------------
+def PlotSpectraLogRatioDataDivSimSmooth(the_filelist,path_tosims,the_obs,the_searchtag,wlshift,the_title,
+                        FLAG_WL_CORRECTION,Flag_corr_wl=False,XMIN=400,XMAX=1000.,YMIN=0,YMAX=0):
+    
+    jet =plt.get_cmap('jet') 
+    VMAX=len(the_filelist)
+    cNorm  = colors.Normalize(vmin=0, vmax=VMAX)
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=jet)
+    
+    the_selected_indexes=the_obs["index"].values  # get the array of index for that disperser
+    
+    plt.figure(figsize=(10,8))
+    num=0
+    for the_file in the_filelist:  # loop on reconstruted spectra
+        num+=1
+        idx=get_index_from_filename(the_file,the_searchtag)
+        
+        if FLAG_WL_CORRECTION and Flag_corr_wl:
+                wl_correction=wlshift[wlshift["index"]==idx].loc[:,"wlshift"].values[0]
+        else:
+                wl_correction=0
+        
+        
+        if idx in the_selected_indexes:                # check if tthe index is in the disperser indexes 
+            basefn=os.path.basename(the_file)                  # basename of reconstruced spectra
+            basefn2=basefn.replace('reduc','specsim')  # reconstruct the simulation filename
+            the_filesim=os.path.join(path_tosims,basefn2)  # add the path for the simulated file
+            
+            hdu1 = fits.open(the_file)
+            data1=hdu1[0].data
+            wl1=data1[0]+wl_correction
+            fl1=data1[1]
+            err1=data1[2]
+            
+            # extend range for (wl1,fl1)
+            wl1=np.insert(wl1,0,WL[0])
+            fl1=np.insert(fl1,0,0.)
+            err1=np.insert(err1,0,0.)
+            
+            wl1=np.append(wl1,WL[-1])
+            fl1=np.append(fl1,0.)
+            err1=np.append(err1,0.)
+            
+            hdu2 = fits.open(the_filesim)
+            data2=hdu2[0].data
+            wl2=data2[0]
+            fl2=data2[1]
+            
+            func = interpolate.interp1d(wl1, fl1)
+            efunc = interpolate.interp1d(wl1, err1) 
+            
+            fl0=func(WL)
+            er0=efunc(WL)
+            
+            f1_smooth=smooth(fl0,window_len=21)
+            f2_smooth=smooth(fl2,window_len=21)
+            
+            #plt.plot(WL,2.5*(np.log10(fl0)-np.log10(fl2)))
+            
+            the_Y=2.5*(np.log10(f1_smooth)-np.log10(f2_smooth))
+            the_Y=f1_smooth/f2_smooth
+            
+            sel_iii=np.where(np.logical_and(WL>=XMIN,WL<=XMAX))[0]
+           
+            the_Y_max=(np.max(the_Y[sel_iii]))*3.0
+            the_Y_min=(np.min(the_Y[sel_iii]))/3.0
+            
+            colorVal = scalarMap.to_rgba(num,alpha=1)
+            
+            plt.semilogy(WL,the_Y,color=colorVal)
+            
+            
+    plt.xlim(XMIN,XMAX)
+    
+    if YMIN==0 and YMAX==0 :
+        print 'the_Y_min,the_Y_max =',the_Y_min,the_Y_max
+        plt.ylim(the_Y_min,the_Y_max)
+    else:
+        plt.ylim(YMIN,YMAX)
+    
+    
+    #plt.grid(True,which="majorminor",ls="-", color='0.65')
+    #plt.grid(True,which="both",ls="-")
+    plt.grid(b=True, which='major', color='k', linestyle='-',lw=1)
+    plt.grid(b=True, which='minor', color='grey', linestyle='--',lw=0.5)
+    
+    plt.title(the_title)
+    plt.xlabel("$\lambda$ (nm)")  
+    plt.ylabel("Spectra ratio (mag)")  
+#------------------------------------------------------------------------------------    
+    
