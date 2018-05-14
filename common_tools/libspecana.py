5a7410f925c1fcfc1acdbf53af1d679d8ce1d584
Sylvie Dagoret-Campagne <dagoret@lal.in2p3.fr>
2018-05-14 21:57:33 +0200

    make fit of bougher lines

--- common_tools/libspecana.py
+++ common_tools/libspecana.py
@@ -13,6 +13,9 @@ import glob
 from matplotlib.backends.backend_pdf import PdfPages 
 from scipy import interpolate
 from astropy.io import fits
+
+from scipy.optimize import curve_fit
+
 import pysynphot as S
 
 import sys,os
@@ -1617,6 +1620,383 @@ def PlotSpectraDataSimAttenuationRatioSmoothBin(the_filelist,the_obs,the_searcht
     plt.show() 
     
 #-------------------------------------------------------------------------------------    
+#   FIT
+#----------------------------------------------------------------------------------    
+#--------------------------------------------------------------------------------------------   
+def bougline(x, a, b):
+    return a*x + b
+
+#---------------------------------------------------------------------------------------------    
+def FitBougherLine(theX,theY,theSigY):
     
+    
+    Xfit=np.linspace(0,theX.max()*1.1,20.)
+    xfit=Xfit
+    
+    theZ = np.polyfit(theX,theY, 1)
+    
+   
+    popt, pcov = curve_fit(bougline, theX, theY,p0=theZ,sigma=theSigY)
+    perr = np.sqrt(np.diag(pcov))
+    
+    #print "popt = ",popt,' pcov',pcov,' perr',perr
+    
+    pol = np.poly1d(popt)
+    
+    Yfit=pol(theX)
+    chi2sum=(Yfit-np.array(theY))**2/np.array(theSigY)**2
+    chi2=np.average(chi2sum)*chi2sum.shape[0]/(chi2sum.shape[0]-3)
+    #print 'chi2',chi2
+    
+    
+    #p = np.poly1d(theZ)
+    #yfit=p(xfit)
+    
+    yfit=pol(xfit)
+    
+    return xfit,yfit
+    
+        
+#------------------------------------------------------------------------------------------
+def FitSpectraDataSimAttenuationRatioSmooth(the_filelist,the_obs,the_searchtag,wlshift,the_title,
+                                        FLAG_WL_CORRECTION,Flag_corr_wl=False,
+                                        XMIN=0,XMAX=0,YMIN=0,YMAX=0,ZMIN=0,ZMAX=0,Wwidth=21,Mag=True):
+
+    
+    # color according wavelength
+    jet =plt.get_cmap('jet') 
+    if (ZMIN==0 and ZMAX==0):
+        WLMIN=300.
+        WLMAX=600.
+    elif ZMIN==0:
+        WLMIN=WL.min()
+        WLMAX=ZMAX
+    elif ZMAX==0:
+        WLMIN=ZMIN
+        WLMAX=600.
+    else:
+        WLMIN=ZMIN
+        WLMAX=ZMAX
+        
+    cNorm  = colors.Normalize(vmin=WLMIN, vmax=WLMAX)
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=jet)
+    
+    the_selected_indexes=the_obs["index"].values  # get the array of index for that disperser
+
+
+    
+    # attenuation rows: the file index, the airmass, the attenuation for each WL
+    attenuation=np.zeros((len(the_filelist)+1,2+len(WL)))
+    att_err=np.zeros((len(the_filelist)+1,2+len(WL)))
+    all_sed=np.zeros((len(the_filelist)+1,2+len(WL)))
+    
+    
+   
+    num=0
+    numsel=0
+    for the_file in the_filelist:
+        num=num+1
+        idx=get_index_from_filename(the_file,the_searchtag)
+        if idx in the_selected_indexes:
+            numsel+=1
+            if FLAG_WL_CORRECTION and Flag_corr_wl:
+                wl_correction=wlshift[wlshift["index"]==idx].loc[:,"wlshift"].values[0]
+            else:
+                wl_correction=0
+            
+            hdu = fits.open(the_file)
+            header=hdu[0].header
+            airmass=header["AIRMASS"]
+            objectname=header["TARGET"]
+            
+            # extract smoothed SED
+            sedsm=GetSEDSmooth(objectname,Wwidth=Wwidth)
+            
+            
+            data=hdu[0].data
+            wl=data[0]+wl_correction
+            fl=data[1]
+            err=data[2]
+            
+            
+            
+            # extend range for (wl1,fl1)
+            wl=np.insert(wl,0,WL[0])
+            fl=np.insert(fl,0,0.)
+            err=np.insert(err,0,0.)
+            
+            wl=np.append(wl,WL[-1])
+            fl=np.append(fl,0.)
+            err=np.append(err,0.)
+            
+            func = interpolate.interp1d(wl, fl)
+            efunc = interpolate.interp1d(wl, err) 
+            
+            fl0=func(WL)
+            er0=efunc(WL)
+            
+            fl_smooth=smooth(fl0,window_len=Wwidth)
+            errfl_smooth=smooth(er0,window_len=Wwidth)
+            
+            attenuation[numsel,0]=idx
+            attenuation[numsel,1]=airmass
+            attenuation[numsel,2:]=fl_smooth            
+            att_err[numsel,2:]=errfl_smooth
+            
+            # smoothed SED
+            all_sed[numsel,0]=idx
+            all_sed[numsel,1]=airmass
+            all_sed[numsel,2:]=sedsm 
+            
+            
+      
+    AIRMASS_MIN=attenuation[1:,1].min()
+    AIRMASS_MAX=attenuation[1:,1].max()
+    
+    all_airmasses=attenuation[1:,1]
+    all_imgidx=attenuation[1:,0]
+    
+    # selection 
+    good_indexes=np.where(attenuation[:,1]>0)[0]
+    
+    
+    sel_attenuation=attenuation[good_indexes,:]
+    sel_airmasses=sel_attenuation[:,1]
+    sel_imgidx=sel_attenuation[:,0]  
+    sel_sed=all_sed[good_indexes,:]
+    
+    airmassmin_index=np.where(sel_airmasses==sel_airmasses.min())[0][0]
+    
+    
+    plt.figure(figsize=(15,8))
+    # loop on wavelength indexes
+    for idx_wl in np.arange(2,len(WL)+2): 
+        if WL[idx_wl-2]<WLMIN:
+            continue
+        if WL[idx_wl-2]>WLMAX:
+            break
+        colorVal = scalarMap.to_rgba(WL[idx_wl-2],alpha=1)
+        att_airmassmin=sel_attenuation[airmassmin_index,idx_wl]
+        
+        if not Mag:
+            plt.semilogy(sel_airmasses,sel_attenuation[:,idx_wl]/sel_sed[:,idx_wl],'o-',c=colorVal)
+        else:
+            plt.plot(sel_airmasses,2.5*np.log10(sel_attenuation[:,idx_wl]/sel_sed[:,idx_wl]),'o-',c=colorVal)
+          
+            
+    
+    plt.grid(b=True, which='major', color='k', linestyle='-',lw=1)
+    plt.grid(b=True, which='minor', color='grey', linestyle='--',lw=0.5)
+    
+    plt.title(the_title)
+    plt.xlabel("airmass")   
+    if not Mag:
+        plt.ylabel("intensity in data/ intensity of SED")  
+    else:
+        plt.ylabel(" 2.5 x log1°(intensity in data/ intensity of SED) (mag)")  
+    plt.xlim(0.,sel_airmasses.max()*1.1)  
+    
+    
+    plt.show() 
+#----------------------------------------------------------------------------------------
+
+
+
+#--------------------------------------------------------------------------------------
+def FitSpectraDataSimAttenuationRatioSmoothBin(the_filelist,the_obs,the_searchtag,wlshift,the_title,
+                                        FLAG_WL_CORRECTION,Flag_corr_wl=False,
+                                        XMIN=0,XMAX=0,YMIN=0,YMAX=0,ZMIN=0,ZMAX=0,Wwidth=21,Bwidth=20,Mag=True):
+
+    
+        # color according wavelength
+    jet =plt.get_cmap('jet') 
+    if (ZMIN==0 and ZMAX==0):
+        WLMIN=300.
+        WLMAX=600.
+    elif ZMIN==0:
+        WLMIN=WL.min()
+        WLMAX=ZMAX
+    elif ZMAX==0:
+        WLMIN=ZMIN
+        WLMAX=600.
+    else:
+        WLMIN=ZMIN
+        WLMAX=ZMAX
+        
+    cNorm  = colors.Normalize(vmin=WLMIN, vmax=WLMAX)
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=jet)
+    
+    the_selected_indexes=the_obs["index"].values  # get the array of index for that disperser
+    
+    #------------------------------------------------------------------------
+    # attenuation container 
+    # attenuation rows: the file index, the airmass, the attenuation for each WL
+    #-------------------------------------------------------------------------
+    attenuation=np.zeros((len(the_filelist)+1,2+len(WL)))
+    att_err=np.zeros((len(the_filelist)+1,2+len(WL)))
+    all_sed=np.zeros((len(the_filelist)+1,2+len(WL)))
+    
+    num=0
+    numsel=0
+    for the_file in the_filelist:
+        num=num+1
+        idx=get_index_from_filename(the_file,the_searchtag)
+        if idx in the_selected_indexes:
+            numsel+=1
+            if FLAG_WL_CORRECTION and Flag_corr_wl:
+                wl_correction=wlshift[wlshift["index"]==idx].loc[:,"wlshift"].values[0]
+            else:
+                wl_correction=0
+            
+            hdu = fits.open(the_file)
+            header=hdu[0].header
+            airmass=header["AIRMASS"]
+            objectname=header["TARGET"]
+            
+            # extract smoothed SED
+            sedsm=GetSEDSmooth(objectname,Wwidth=Wwidth)
+            
+            
+            data=hdu[0].data
+            wl=data[0]+wl_correction
+            fl=data[1]
+            err=data[2]
+            
+            
+            
+            # extend range for (wl1,fl1)
+            wl=np.insert(wl,0,WL[0])
+            fl=np.insert(fl,0,0.)
+            err=np.insert(err,0,0.)
+            
+            wl=np.append(wl,WL[-1])
+            fl=np.append(fl,0.)
+            err=np.append(err,0.)
+            
+            func = interpolate.interp1d(wl, fl)
+            efunc = interpolate.interp1d(wl, err) 
+            
+            fl0=func(WL)
+            er0=efunc(WL)
+            
+            fl_smooth=smooth(fl0,window_len=Wwidth)
+            errfl_smooth=smooth(er0,window_len=Wwidth)
+            
+            # attenuation in data
+            attenuation[numsel,0]=idx
+            attenuation[numsel,1]=airmass
+            attenuation[numsel,2:]=fl_smooth  
+            
+            # error on attenuation
+            att_err[numsel,2:]=errfl_smooth
+            
+            # smoothed SED
+            all_sed[numsel,0]=idx
+            all_sed[numsel,1]=airmass
+            all_sed[numsel,2:]=sedsm 
+      
+    AIRMASS_MIN=attenuation[1:,1].min()
+    AIRMASS_MAX=attenuation[1:,1].max()
+    
+    all_airmasses=attenuation[1:,1]
+    all_imgidx=attenuation[1:,0]
+    
+    
+    
+    # selection where airmass are OK
+    #---------------------------------
+    good_indexes=np.where(attenuation[:,1]>0)[0]
+    
+    
+    sel_attenuation=attenuation[good_indexes,:]
+    sel_err=att_err[good_indexes,:]
+    sel_airmasses=sel_attenuation[:,1]
+    sel_imgidx=sel_attenuation[:,0]
+    
+    sel_sed=all_sed[good_indexes,:]
+    
+    
+
+    
+    airmassmin_index=np.where(sel_airmasses==sel_airmasses.min())[0][0]
+    #print 'airmass-min = ',sel_airmasses[airmassmin_index]
+    
+    # loop on wavelength bins
+    #plt.figure(figsize=(15,4))
+    #plt.plot(sel_imgidx,sel_airmasses,'o')
+    #plt.show()
+    
+    ################### Plot the figure ###############################################
+    plt.figure(figsize=(15,8))
+    # loop on wavelength indexes
+    for idx_wl in np.arange(2,len(WL)+2,Bwidth): 
+        if WL[idx_wl-2]<WLMIN:
+            continue
+        if WL[idx_wl-2]>WLMAX:
+            break
+        
+        colorVal = scalarMap.to_rgba(WL[idx_wl-2],alpha=1)
+        idx_startwl=idx_wl
+        idx_stopwl=min(idx_wl+Bwidth-1,sel_attenuation.shape[1])
+        
+        thelabel="{:d}-{:d} nm".format(WL[idx_startwl-2],WL[idx_stopwl-2] )
+        
+        # slice of  flux in wavelength bins
+        FluxBin=sel_attenuation[:,idx_startwl:idx_stopwl]
+        FluxBinSED=sel_sed[:,idx_startwl:idx_stopwl]
+        
+        FluxBinErr=sel_err[:,idx_startwl:idx_stopwl]
+       
+                 
+        # get the average of flux in that big wl bin
+        FluxAver=np.average(FluxBin,axis=1)
+        FluxAverSED=np.average(FluxBinSED,axis=1)
+        FluxAverErr=np.average(FluxBinErr,axis=1)
+        
+        Y0=FluxAver/FluxAverSED
+        Y1=Y0-FluxAverErr/FluxAverSED
+        Y2=Y0+FluxAverErr/FluxAverSED
+        
+        # get the attenuation for the airmass-min
+        att_airmassmin=sel_attenuation[airmassmin_index,idx_wl]
+        
+        if not Mag:        
+            plt.fill_between(sel_airmasses,y1=Y1,y2=Y2, where=Y1>0 ,color='grey', alpha=0.3 )        
+            plt.yscale( "log" )
+        
+            # plot the attenuation wrt airmass
+            plt.semilogy(sel_airmasses,Y0,'o-',c=colorVal,label=thelabel)
+        else:
+            
+            newY0=2.5*np.log10(Y0)
+            newY1=2.5*np.log10(Y1)
+            newY2=2.5*np.log10(Y2)
+            
+            plt.fill_between(sel_airmasses,y1=newY1,y2=newY2, where=Y1>0 ,color='grey', alpha=0.3 )        
+            
+            # plot the attenuation wrt airmass
+            plt.plot(sel_airmasses,newY0,'o-',c=colorVal,label=thelabel)
+            
+            Xfit,Yfit=FitBougherLine(sel_airmasses,newY0,theSigY=(newY2-newY1)/2.)
+            plt.plot(Xfit,Yfit,'-',c=colorVal)
+            
+    
+    plt.grid(b=True, which='major', color='k', linestyle='-',lw=1)
+    plt.grid(b=True, which='minor', color='grey', linestyle='--',lw=0.5)
+    plt.title(the_title)
+    plt.xlabel("airmass")   
+    if not Mag:  
+        plt.ylabel("intensity in data / intensity in SED") 
+    else:
+        plt.ylabel("intensity in data / intensity in SED (mag)") 
+    plt.legend(loc='right')  
+    
+    if XMAX==0:
+        plt.xlim(0.,sel_airmasses.max()*1.3)
+    else:
+        plt.xlim(0.,XMAX)
+    plt.show() 
+    
+#-------------------------------------------------------------------------------------        
     
     
